6a3b6287b24be06ea75cd0442086aaf32da8f288
PremSharma1995 <premsharma9766@gmail.com>
2024-05-01 18:25:35 +0530

    Initial Commit

---
+++ src/main/java/com/crick/apis/CricketLiveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.crick.apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketLiveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketLiveBackendApplication.class, args);
+	}
+
+}
